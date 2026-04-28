a7098a2bdbbee0167428a73b6d16102512c03186
chilakala5 <venugopalr.chilakala@gmail.com>
2026-04-29 02:38:41 +0530

    Initial commit: add hello world script and config

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+/**
+ * Returns a friendly greeting.
+ * @param {string} name
+ * @returns {string}
+ */
+function greet(name) {
+  return `Hello, ${name}! Welcome to temp-repo.`;
+}
+
+module.exports = { greet };
